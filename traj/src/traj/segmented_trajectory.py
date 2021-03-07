e95c639568da3bd0299542bd495c6f69d41fae49
Jon Binney <jon.binney@gmail.com>
2021-03-07 14:16:05 -0500

    Jupyter notebook for exploring motion equations

--- traj/src/traj/segmented_trajectory.py
+++ traj/src/traj/segmented_trajectory.py
@@ -2,6 +2,7 @@ import numpy as np
 import scipy.optimize
 import sympy
 
+
 def generate_trajectory_equations(jerk_directions):
     j_max = sympy.Symbol('j_max')
     jerk_arr = []
@@ -18,11 +19,21 @@ def generate_trajectory_equations(jerk_directions):
         velocity += acceleration * delta + sympy.Rational(1, 2) * jerk * delta ** 2
         acceleration += jerk * delta
 
-    state_eq = sympy.Matrix([position, velocity, acceleration])
+    state_eq = sympy.Matrix([
+        sympy.Eq(sympy.Symbol('p_end'), position),
+        sympy.Eq(sympy.Symbol('v_end'), velocity),
+        sympy.Eq(sympy.Symbol('a_end'), acceleration)])
     return delta_variables, state_eq
 
+
 class SegmentedTrajectory:
-    def __init__(self, j_max, jerk_directions, p_start, v_start, a_start, p_end, v_end, a_end):
+    def __init__(self,
+            v_max, a_max, j_max,
+            jerk_directions,
+            p_start, v_start, a_start,
+            p_end, v_end, a_end):
+        self.v_max = v_max
+        self.a_max = a_max
         self.j_max = j_max
         self.jerk_directions = jerk_directions
         self.p_start = p_start
@@ -31,30 +42,35 @@ class SegmentedTrajectory:
         self.p_end = p_end
         self.v_end = v_end
         self.a_end = a_end
-        self.delta_values = None # Unknown until self.optimize() is called.
+        self.delta_values = None  # Unknown until self.optimize() is called.
         self.delta_variables, self.state_eq = generate_trajectory_equations(self.jerk_directions)
-        subs = {
+        self.subs = {
+            # We don't need v_max and a_max here because they are unnecessary once we know
+            # the basic shape of the acceleration, which is implied by the jerk directions.
             sympy.Symbol('j_max'): self.j_max,
             sympy.Symbol('p_start'): self.p_start,
             sympy.Symbol('v_start'): self.v_start,
             sympy.Symbol('a_start'): self.a_start,
+            sympy.Symbol('p_end'): self.p_end,
+            sympy.Symbol('v_end'): self.v_end,
+            sympy.Symbol('a_end'): self.a_end,
         }
-        self.state_lambda = sympy.lambdify(self.delta_variables, self.state_eq.subs(subs))
-        self.state_jacobian_eq = self.state_eq.subs(subs).jacobian(self.delta_variables)
+        self.state_lambda = sympy.lambdify(self.delta_variables, self.state_eq.subs(self.subs))
+        self.state_jacobian_eq = self.state_eq.subs(self.subs).jacobian(self.delta_variables)
         self.state_jacobian_lambda = sympy.lambdify(self.delta_variables, self.state_jacobian_eq)
 
     def compute_end_state(self, delta_values):
-        return self.state_lambda(*delta_values).flat
+        return self.state_lambda(*delta_values).flatten()
 
     def error_jacobian(self, x):
-        delta_values = np.asarray(x)**2
+        delta_values = np.asarray(x) ** 2
         return self.state_jacobian_lambda(*delta_values)
 
     def error_function(self, x):
         x = np.asarray(x)
 
         # Durations must be positive.
-        delta_values = x**2
+        delta_values = x ** 2
 
         p_end_est, v_end_est, a_end_est = self.compute_end_state(delta_values)
 
@@ -70,4 +86,23 @@ class SegmentedTrajectory:
             tol=tolerance)
         if not solution.success:
             raise RuntimeError('Optimization failed')
-        self.delta_values = solution.x**2
+        self.delta_values = solution.x ** 2
+
+    def optimize2(self):
+        subs = {
+            sympy.Symbol('j_max'): self.j_max,
+            sympy.Symbol('p_start'): self.p_start,
+            sympy.Symbol('v_start'): self.v_start,
+            sympy.Symbol('a_start'): self.a_start,
+            sympy.Symbol('delta_2'): 1.0,
+        }
+        concrete_state_eq = self.state_eq.subs(subs)
+        print(sympy.pretty(self.state_eq[0], use_unicode=True))
+        print(sympy.pretty(self.state_eq[1], use_unicode=True))
+        print(sympy.pretty(self.state_eq[2], use_unicode=True))
+        print('sdlkfj'*30)
+        import os
+        rows, columns = os.popen('stty size', 'r').read().split()
+        print(columns)
+
+        # Solve acceleration equation for delta_1
