154c1a8fce0c9234354453e6b01a976a3a136e2b
Jon Binney <jon.binney@gmail.com>
2021-03-24 11:49:47 -0400

    Incremental work on seven segment solver
    Still a long way to go. Made progress on trajectories that never reach
    max acceleration or max velocity. Stubbed out a general function for
    solving all trajectory types. I'm pausing this line of work while I
    check out ruckig, which may do all of this.

--- traj/src/traj/segmented_trajectory.py
+++ traj/src/traj/segmented_trajectory.py
@@ -1,108 +1,121 @@
+from enum import Enum
+import time
+
 import numpy as np
-import scipy.optimize
 import sympy
 
+VALID_TRAJECTORY_TYPES = {
+    'jaJAj': 'PosTrapNegTrap',
+    'jaJj': 'PosTrapNegTri',
+    'jJAj': 'PosTriNegTrap',
+    'jJj': 'PosTriNegTri',
+    'JAjaJ': 'NegTrapPosTrap',
+    'JAjJ': 'NegTrapPosTri',
+    'JjaJ': 'NegTriPosTrap',
+    'JjJ': 'NegTriPosTri',
+}
 
-def generate_trajectory_equations(jerk_directions):
+
+def generate_motion_equations(jerk_directions):
     j_max = sympy.Symbol('j_max')
     jerk_arr = []
     for dir in jerk_directions:
         jerk_arr.append({1: j_max, 0: 0.0, -1: -j_max}[dir])
-    delta_variables = [sympy.Symbol('delta_{}'.format(i)) for i in range(len(jerk_arr))]
+    delta_symbols = [sympy.Symbol('delta_{}'.format(i)) for i in range(len(jerk_arr))]
     position = sympy.Symbol('p_start')
     velocity = sympy.Symbol('v_start')
     acceleration = sympy.Symbol('a_start')
     for i, jerk in enumerate(jerk_arr):
-        delta = delta_variables[i]
+        delta = delta_symbols[i]
         position += velocity * delta + sympy.Rational(1, 2) * acceleration * delta ** 2 + \
                     sympy.Rational(1, 6) * jerk * delta ** 3
         velocity += acceleration * delta + sympy.Rational(1, 2) * jerk * delta ** 2
         acceleration += jerk * delta
 
-    state_eq = sympy.Matrix([
+    motion_equations = sympy.Matrix([
         sympy.Eq(sympy.Symbol('p_end'), position),
         sympy.Eq(sympy.Symbol('v_end'), velocity),
         sympy.Eq(sympy.Symbol('a_end'), acceleration)])
-    return delta_variables, state_eq
+    return motion_equations
 
 
-class SegmentedTrajectory:
-    def __init__(self,
-            v_max, a_max, j_max,
-            jerk_directions,
-            p_start, v_start, a_start,
-            p_end, v_end, a_end):
-        self.v_max = v_max
-        self.a_max = a_max
-        self.j_max = j_max
-        self.jerk_directions = jerk_directions
-        self.p_start = p_start
-        self.v_start = v_start
-        self.a_start = a_start
-        self.p_end = p_end
-        self.v_end = v_end
-        self.a_end = a_end
-        self.delta_values = None  # Unknown until self.optimize() is called.
-        self.delta_variables, self.state_eq = generate_trajectory_equations(self.jerk_directions)
-        self.subs = {
-            # We don't need v_max and a_max here because they are unnecessary once we know
-            # the basic shape of the acceleration, which is implied by the jerk directions.
-            sympy.Symbol('j_max'): self.j_max,
-            sympy.Symbol('p_start'): self.p_start,
-            sympy.Symbol('v_start'): self.v_start,
-            sympy.Symbol('a_start'): self.a_start,
-            sympy.Symbol('p_end'): self.p_end,
-            sympy.Symbol('v_end'): self.v_end,
-            sympy.Symbol('a_end'): self.a_end,
-        }
-        self.state_lambda = sympy.lambdify(self.delta_variables, self.state_eq.subs(self.subs))
-        self.state_jacobian_eq = self.state_eq.subs(self.subs).jacobian(self.delta_variables)
-        self.state_jacobian_lambda = sympy.lambdify(self.delta_variables, self.state_jacobian_eq)
+def solve_seven_segment(
+        segment_types, v_max, a_max, j_max, p_start, v_start, a_start, p_end, v_end, a_end):
+    deltas = np.tile(np.nan, len(segment_types))
+    intermediate_states = np.tile(np.nan, (len(segment_types) + 1, 3))
+    intermediate_states[0, :] = (p_start, v_start, a_start)
+    intermediate_states[-1, :] = (p_end, v_end, a_end)
 
-    def compute_end_state(self, delta_values):
-        return self.state_lambda(*delta_values).flatten()
+    # Compute first/last segment durations for a_max and -a_max cases.
+    if segment_types[1] == 'a':
+        deltas[0] = (a_max - intermediate_states[0, 2]) / j_max
+    elif segment_types[1] == 'A':
+        deltas[0] = (-a_max - intermediate_states[0, 2]) / -j_max
+    if segment_types[-2] == 'a':
+        deltas[-1] = (intermediate_states[-1, 2] - a_max) / j_max
+    elif segment_types[-2] == 'A':
+        deltas[0] = (intermediate_states[-1, 2] + a_max) / -j_max
 
-    def error_jacobian(self, x):
-        delta_values = np.asarray(x) ** 2
-        return self.state_jacobian_lambda(*delta_values)
+    # If there is a max velocity segment, solve for the segments next to it
 
-    def error_function(self, x):
-        x = np.asarray(x)
+# At import tarrayime, we compute the basic motion equations.
+p_end_eq, v_end_eq, a_end_eq = generate_motion_equations([1, -1, 1])
+delta_symbols = sympy.symbols('delta_0, delta_1, delta_2')
+param_symbols = sympy.symbols('j_max, p_start, v_start, a_start, p_end, v_end, a_end')
+p_end_lambda = sympy.lambdify(delta_symbols + param_symbols, p_end_eq.rhs)
+v_end_lambda = sympy.lambdify(delta_symbols + param_symbols, v_end_eq.rhs)
+a_end_lambda = sympy.lambdify(delta_symbols + param_symbols, a_end_eq.rhs)
+# Solve acceleration equation for delta_0. The acceleration equation is linear,
+# so this only has one solution.
+delta_0_solution = sympy.solve(a_end_eq, delta_symbols[0])[0]
+delta_0_lambda = sympy.utilities.lambdify(
+    (delta_symbols[1], delta_symbols[2]) + param_symbols, delta_0_solution)
+# Plug in solution for delta_0 into velocity equation, then solve for delta_1. The velocity
+# equations is quadratic, so there are at most two solutions.
+#
+# Note: is one of the solutions always negative? Could we remove that one here to simplify
+# things later?
+delta_1_solutions = sympy.solve(
+    v_end_eq.subs(delta_symbols[0], delta_0_solution), delta_symbols[1])
+delta_1_lambdas = [sympy.utilities.lambdify((delta_symbols[2],) + param_symbols, solution)
+                   for solution in delta_1_solutions]
 
-        # Durations must be positive.
-        delta_values = x ** 2
 
-        p_end_est, v_end_est, a_end_est = self.compute_end_state(delta_values)
+def compute_delta_0_and_1(delta_2, j_max, p_start, v_start, a_start, p_end, v_end, a_end):
+    for delta_1_lambda in delta_1_lambdas:
+        delta_1 = delta_1_lambda(delta_2, j_max, p_start, v_start, a_start, p_end, v_end, a_end)
+        delta_0 = delta_0_lambda(delta_1, delta_2, j_max, p_start, v_start,
+                                 a_start, p_end, v_end, a_end)
+        if delta_0 >= 0 and delta_1 >= 0:
+            return delta_0, delta_1
+    raise RuntimeError(f'No valid delta_0 and delta_1 values for delta_2={delta_2}')
 
-        errors = np.array((p_end_est - self.p_end, v_end_est - self.v_end, a_end_est - self.a_end))
-        if not np.isfinite(errors).all():
-            raise RuntimeError('Non-finite values in trajectory error')
-        return errors
 
-    def optimize(self, tolerance=1e-6):
-        delta_values_guess = np.random.random(len(self.delta_variables))
-        solution = scipy.optimize.root(
-            self.error_function, delta_values_guess, jac=self.error_jacobian, method='hybr',
-            tol=tolerance)
-        if not solution.success:
-            raise RuntimeError('Optimization failed')
-        self.delta_values = solution.x ** 2
+def solve(v_max, a_max, j_max, p_start, v_start, a_start, p_end, v_end, a_end,
+          tolerance=1e-5, max_iterations=100):
+    optimization_start_time = time.time()
+    iterations = []
+    params = (j_max, p_start, v_start, a_start, p_end, v_end, a_end)
+    delta_2_min = 0.0
+    delta_2_max = 2.0 * a_max / j_max
+    for iteration_i in range(max_iterations):
+        delta_2 = (delta_2_min + delta_2_max) / 2.0
+        delta_0, delta_1 = compute_delta_0_and_1(delta_2, *params)
+        p_end_calculated = p_end_lambda(delta_0, delta_1, delta_2, *params)
+        iterations.append((delta_2, p_end_calculated))
+        position_error = np.abs(p_end_calculated - p_end)
+        if position_error <= tolerance:
+            optimization_end_time = time.time()
+            optimization_info = {
+                'duration': optimization_end_time - optimization_start_time,
+                'iterations': iterations,
+                'position_error': position_error
+            }
+            return (delta_0, delta_1, delta_2), optimization_info
 
-    def optimize2(self):
-        subs = {
-            sympy.Symbol('j_max'): self.j_max,
-            sympy.Symbol('p_start'): self.p_start,
-            sympy.Symbol('v_start'): self.v_start,
-            sympy.Symbol('a_start'): self.a_start,
-            sympy.Symbol('delta_2'): 1.0,
-        }
-        concrete_state_eq = self.state_eq.subs(subs)
-        print(sympy.pretty(self.state_eq[0], use_unicode=True))
-        print(sympy.pretty(self.state_eq[1], use_unicode=True))
-        print(sympy.pretty(self.state_eq[2], use_unicode=True))
-        print('sdlkfj'*30)
-        import os
-        rows, columns = os.popen('stty size', 'r').read().split()
-        print(columns)
+        if p_end_calculated < p_end:
+            delta_2_min = delta_2
+        else:
+            delta_2_max = delta_2
 
-        # Solve acceleration equation for delta_1
+    raise RuntimeError(f'Error of {position_error} too high after {max_iterations} iterations')
--- traj/test/test_segmented_trajectory.py
+++ traj/test/test_segmented_trajectory.py
@@ -1,37 +1,39 @@
 import time
 import numpy as np
 
-import traj.segmented_trajectory
+from traj import segmented_trajectory
 
+def compare_solution_to_prediction(
+        jerk_directions, v_max, a_max, j_max, p_start, v_start, a_start,
+        p_end, v_end, a_end, actual_deltas):
+    calculated_deltas, optimization_info = segmented_trajectory.solve(
+        v_max, a_max, j_max, p_start, v_start, a_start, p_end, v_end, a_end)
+    print(f'Solved for trajectory in {int(round(optimization_info["duration"]*1e6))}us'
+          + f'({len(optimization_info["iterations"])} iterations)')
+    params = (j_max, p_start, v_start, a_start, p_end, v_end, a_end)
+    p_end_calculated = segmented_trajectory.p_end_lambda(*calculated_deltas, *params)
+    v_end_calculated = segmented_trajectory.v_end_lambda(*calculated_deltas, *params)
+    a_end_calculated = segmented_trajectory.a_end_lambda(*calculated_deltas, *params)
 
-def test_zero_length_trajectory():
-    trajectory = traj.segmented_trajectory.SegmentedTrajectory(
-        1.0, [1, -1, 1], 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
-    error = trajectory.error_function([0.0, 0.0, 0.0])
-    assert np.isclose(error, (0.0,)).all()
+    assert np.isclose(p_end_calculated, p_end, atol=1e5)
+    assert np.isclose(v_end_calculated, v_end, atol=1e5)
+    assert np.isclose(a_end_calculated, a_end, atol=1e5)
 
-    trajectory.optimize()
-    error = trajectory.error_function(trajectory.delta_values)
-    assert np.isclose(error, (0.0,)).all()
+def test_nonzero_ending_velocity_and_acceleration():
+    compare_solution_to_prediction(
+        (1, -1, 1), 2.0, 3.0, 1.0,
+        0.0, 0.0, 0.0,
+        2.166666666666667, 1.5, 1.0,
+        actual_deltas=(1., 1., 1.))
 
-    print(trajectory.delta_values)
-    assert np.isclose(trajectory.delta_values, (0.0, 0.0, 0.0), atol=1e-3).all()
+def test_zero_trajectory():
+    compare_solution_to_prediction(
+        (1, -1, 1), 2.0, 3.0, 1.0,
+        0.0, 0.0, 0.0,
+        0.0, 0.0, 0.0, actual_deltas=(0.0, 0.0, 0.0))
 
-def test_single_segment_trajectory():
-   trajectory = traj.segmented_trajectory.SegmentedTrajectory(
-       1.0, [1], 0.0, 0.0, 0.0, 1./6., 0.5, 1.0)
-
-   # With the correct duration, there should be no error.
-   error = trajectory.error_function([1.0])
-   assert np.allclose(error, (0.0, 0.0, 0.0))
-
-   assert np.allclose(trajectory.compute_end_state([0.0]), (0.0, 0.0, 0.0))
-   assert np.allclose(trajectory.compute_end_state([2.0]), (4./3., 2.0, 2.0))
-
-def test_two_segment_trajectory():
-   trajectory = traj.segmented_trajectory.SegmentedTrajectory(
-      1.0, [1, -1], 0.0, 0.0, 0.0, 0.0, 0.5, 1.0)
-
-   assert np.allclose(trajectory.compute_end_state([0.0, 0.0]), (0.0,  0.0, 0.0))
-   assert np.allclose(trajectory.compute_end_state([1.0, 2.0]), (11./6.,  0.5, -1.0))
-
+def test_simple_trajectory():
+    compare_solution_to_prediction(
+        (1, -1, 1), 2.0, 3.0, 1.0,
+        0.0, 0.0, 0.0,
+        2.0, 0.0, 0.0, actual_deltas=(1.0, 2.0, 1.0))
